f98a9e38fa3de59977b0622353348e32c4f32e4b
83891 <a83891@83891.local>
2025-04-24 17:17:03 +0900

    feat: test change image

--- packages/frontend/@n8n/design-system/src/components/N8nAvatar/Avatar.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nAvatar/Avatar.vue
@@ -1,14 +1,10 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-import Avatar from 'vue-boring-avatars';
-
-import { getInitials } from '../../utils/labelUtil';
+import LogoPlayground from './Logo_playground_text.svg';
 
 interface AvatarProps {
 	firstName?: string;
 	lastName?: string;
 	size?: 'xsmall' | 'small' | 'medium' | 'large';
-	colors?: string[];
 }
 
 defineOptions({ name: 'N8nAvatar' });
@@ -16,22 +12,7 @@ const props = withDefaults(defineProps<AvatarProps>(), {
 	firstName: '',
 	lastName: '',
 	size: 'medium',
-	colors: () => [
-		'--color-primary',
-		'--color-secondary',
-		'--color-avatar-accent-1',
-		'--color-avatar-accent-2',
-		'--color-primary-tint-1',
-	],
 });
-
-const name = computed(() => `${props.firstName} ${props.lastName}`.trim());
-const initials = computed(() => getInitials(name.value));
-
-const getColors = (colors: string[]): string[] => {
-	const style = getComputedStyle(document.body);
-	return colors.map((color: string) => style.getPropertyValue(color));
-};
 
 const sizes: { [size: string]: number } = {
 	xsmall: 20,
@@ -44,17 +25,9 @@ const getSize = (size: string): number => sizes[size];
 
 <template>
 	<span :class="['n8n-avatar', $style.container]" v-bind="$attrs">
-		<Avatar
-			v-if="name"
-			:size="getSize(size)"
-			:name="name"
-			variant="marble"
-			:colors="getColors(colors)"
-		/>
-		<div v-else :class="[$style.empty, $style[size]]"></div>
-		<span v-if="firstName || lastName" :class="[$style.initials, $style[`text-${size}`]]">
-			{{ initials }}
-		</span>
+		<div :class="[$style.avatar, $style[size]]">
+			<LogoPlayground />
+		</div>
 	</span>
 </template>
 
@@ -64,30 +37,22 @@ const getSize = (size: string): number => sizes[size];
 	display: inline-flex;
 	justify-content: center;
 	align-items: center;
-
-	svg {
-		border-radius: 50%;
-	}
+	background: white;
 }
 
-.empty {
-	display: block;
+.avatar {
 	border-radius: 50%;
-	background-color: var(--color-foreground-dark);
-	opacity: 0.3;
-}
+	background: white;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	overflow: hidden;
 
-.initials {
-	position: absolute;
-	font-size: var(--font-size-2xs);
-	font-weight: var(--font-weight-bold);
-	color: var(--color-avatar-font);
-	text-shadow: 0 1px 6px rgba(25, 11, 9, 0.3);
-	text-transform: uppercase;
-}
-
-.text-xsmall {
-	font-size: 6px;
+	:deep(svg) {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+	}
 }
 
 .xsmall {
