c70cdd06b1ca934ea73c802894e53d6fd0b74ede
JHSeo <qnfqnfqnf@gmail.com>
2025-05-09 16:53:25 +0900

    feat: n8n-bc의 docker-compose.yml 및 Dockerfile 수정, 사용자 정의 엔트리포인트 스크립트 추가

---
+++ docker/images/n8n-bc/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ -d /opt/custom-certificates ]; then
+  echo "Trusting custom certificates from /opt/custom-certificates."
+  export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
+  export SSL_CERT_DIR=/opt/custom-certificates
+  c_rehash /opt/custom-certificates
+fi
+
+# Update the docker group ID to match the host
+if [ -e /var/run/docker.sock ]; then
+	echo "Setting permissions of the docker socket file"
+	sudo chown node:node /var/run/docker.sock
+fi
+
+if [ "$#" -gt 0 ]; then
+  # Got started with arguments
+  exec n8n "$@"
+else
+  # Got started without arguments
+  exec n8n
+fi
